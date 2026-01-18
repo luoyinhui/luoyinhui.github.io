8eae33b04c098240444f188e1b6223310e042342
luoyinhui <yluo3383@gmail.com>
2026-01-18 22:19:11 +0800

    Site updated: 2026-01-18 22:19:06

--- js/swiper/swiper_init.js
+++ js/swiper/swiper_init.js
@@ -1,28 +1,46 @@
-var swiper = new Swiper('.blog-slider', {
+// Prevent duplicate initialization
+if (typeof butterflySwiper === 'undefined') {
+  var butterflySwiper = null;
+}
+
+if (butterflySwiper) {
+  butterflySwiper.destroy(true, true);
+  butterflySwiper = null;
+}
+
+var butterflySwiper = new Swiper('.blog-slider', {
     passiveListeners: true,
-    spaceBetween: 30,
-    effect: 'fade',
+    spaceBetween: 0,
+    effect: 'slide',
+    speed: 600,
     loop: true,
     autoplay: {
       disableOnInteraction: false,
-      delay: 3000
+      delay: 5000
     },
-    mousewheel: true,
-    // autoHeight: true,
+    mousewheel: {
+        forceToAxis: true,
+        sensitivity: 1,
+        thresholdDelta: 50,
+        eventsTarget: '.blog-slider'
+    },
+    // Removed observer for performance
+    // observer: true, 
+    // observeParents: true,
     pagination: {
       el: '.blog-slider__pagination',
       clickable: true,
     },
     navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}
-  });
-  
-  // Optional: Pause on hover
-  var comtainer = document.getElementById('swiper_container');
-  if (comtainer) {
-    comtainer.onmouseenter = function() {
-      swiper.autoplay.stop();
-    };
-    comtainer.onmouseleave = function() {
-      swiper.autoplay.start();
-    };
-  }
+});
+
+// Optional: Pause on hover
+var comtainer = document.getElementById('swiper_container');
+if (comtainer) {
+  comtainer.onmouseenter = function() {
+    if(butterflySwiper) butterflySwiper.autoplay.stop();
+  };
+  comtainer.onmouseleave = function() {
+    if(butterflySwiper) butterflySwiper.autoplay.start();
+  };
+}
