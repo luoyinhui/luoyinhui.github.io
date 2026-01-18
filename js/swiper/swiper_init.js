a200d40d622b6dffdc6516c997311d22b7492f06
luoyinhui <yluo3383@gmail.com>
2026-01-18 16:27:24 +0800

    Site updated: 2026-01-18 16:27:22

---
+++ js/swiper/swiper_init.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper('.blog-slider', {
+    passiveListeners: true,
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 3000
+    },
+    mousewheel: true,
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    },
+    navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}
+  });
+  
+  // Optional: Pause on hover
+  var comtainer = document.getElementById('swiper_container');
+  if (comtainer) {
+    comtainer.onmouseenter = function() {
+      swiper.autoplay.stop();
+    };
+    comtainer.onmouseleave = function() {
+      swiper.autoplay.start();
+    };
+  }
